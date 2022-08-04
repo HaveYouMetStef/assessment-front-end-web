bd2c70cef2a3a4c48e5cd8eac9ea02a97dc82238
Stef Castillo <estefan.castillo@yahoo.com>
2022-08-04 15:48:12 -0500

    Edit JavaScript Commit

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submit');
+}
+
+function hoverPic(evt) {
+	evt.preventDefault();
+
+	alert('give the user a compliment')
 }
 
 
 let form = document.querySelector('#contact');
+let hoverOver= document.querySelector('.catPhoto')
 
 form.addEventListener('submit', handleSubmit);
+
+hoverOver.addEventListener('mouseover', hoverPic)
+
